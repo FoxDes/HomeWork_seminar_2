c0f263177f1a83e09f415444dcd5600b34b7e039
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-11 17:20:40 +0500

    Выполнил первую задачу по ДЗ

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string list = Convert.ToString(a);
+
+Console.WriteLine(list[1]);
