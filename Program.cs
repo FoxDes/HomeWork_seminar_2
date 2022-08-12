ebab165ed25663db0d010f915b613ce0a05943ce
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-12 11:58:53 +0500

    Выполнили вторую(13) задачу по ДЗ

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
 // и на выходе показывает вторую цифру этого числа.
 
+// Console.Clear();
+// Console.Write("Введите трехзначное число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// string list = Convert.ToString(a);
+
+// Console.WriteLine(list[1]);
+
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
 Console.Clear();
-Console.Write("Введите трехзначное число: ");
+Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 string list = Convert.ToString(a);
+int b = 123;
+string c = Convert.ToString(b);
 
-Console.WriteLine(list[1]);
+if (a >= b)
+{
+    Console.WriteLine("Третья цифра заданного числа" + " " + list[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
