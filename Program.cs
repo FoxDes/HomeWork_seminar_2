12e706a86c01bccdd4f9f685e421c39e5784bac6
Denis Malyarenko <mdn-fox@yandex.ru>
2022-08-12 13:07:42 +0500

    выполнил третью(15) задачу по ДЗ

--- Program.cs
+++ Program.cs
@@ -13,18 +13,36 @@
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
 // что третьей цифры нет.
 
-Console.Clear();
-Console.Write("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-string list = Convert.ToString(a);
-int b = 123;
-string c = Convert.ToString(b);
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// string list = Convert.ToString(a);
+// int b = 123;
+// string c = Convert.ToString(b);
 
-if (a >= b)
+// if (a >= b)
+// {
+//     Console.WriteLine("Третья цифра заданного числа" + " " + list[2]);
+// }
+// else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Clear();
+Console.WriteLine("Введите день(число) недели(1-7): ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 5;
+
+if (a <= count)
 {
-    Console.WriteLine("Третья цифра заданного числа" + " " + list[2]);
+    Console.WriteLine("Это рабочий день");
 }
 else
 {
-    Console.WriteLine("Третьей цифры нет");
+    Console.WriteLine("Это выходной день");
 }
